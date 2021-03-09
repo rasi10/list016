83d96829d950ec9dd53da775d68506b3d5ad9b2e
Rafael Silva <rasilva.1986@gmail.com>
2021-03-09 01:50:57 -0800

    Created class OtherGender

---
+++ src/list016/OtherGender.java
@@ -0,0 +1,14 @@
+package list016;
+
+public class OtherGender extends Person{
+	private static final String CLASS_NAME = "Other Gender";
+	
+	public OtherGender(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void printClassName() {
+		System.out.println("The person "+ name +"belongs to class: "+ CLASS_NAME);
+	}
+}
